ada8ca5e479bf62b3c3bb00925fbb125794512f8
o_thollas <o_thollas@hotmail.com>
2023-09-04 10:57:32 +0200

    commentaire sur le model

--- model/Auth.model.js
+++ model/Auth.model.js
@@ -1,20 +1,25 @@
+// Importer le module mongoose pour la création du modèle
 const mongoose = require("mongoose")
 
+// Créer un schéma (ou modèle) pour les utilisateurs
 const Schema = mongoose.Schema;
 
+// Définir le schéma pour les utilisateurs
 const AuthSchema = new Schema({
-    id: Number,
-    fname: String,
-    name: String,
+    id: Number,         // Champ pour l'identifiant de l'utilisateur (peut-être redondant avec _id généré par MongoDB)
+    fname: String,      // Champ pour le prénom de l'utilisateur
+    name: String,      // Champ pour le nom de l'utilisateur
     email: {
         type: String,
-        unique: true,
+        unique: true,   // Assure que l'adresse e-mail est unique dans la base de données
     },
-    password: String,
-    age: Number,
-    location: String,
+    password: String,   // Champ pour le mot de passe haché de l'utilisateur
+    age: Number,        // Champ pour l'âge de l'utilisateur
+    location: String,   // Champ pour l'emplacement de l'utilisateur
 })
 
+// Créer un modèle Mongoose à partir du schéma, nommé "Auth" qui sera utilisé pour interagir avec la base de données
 const AuthModel = mongoose.model("Auth", AuthSchema)
 
+// Exporter le modèle pour pouvoir l'utiliser dans d'autres parties de l'application
 module.exports = AuthModel
--- model/Quizz.model.js
+++ model/Quizz.model.js
@@ -1,23 +1,30 @@
 const mongoose = require("mongoose")
 
+// Importer le module mongoose pour la création du modèle
+
 const Schema = mongoose.Schema;
 
+// Créer un schéma (ou modèle) pour les quiz
 const QuizzSchema = new Schema({
-    title: String,
-    theme: String,
-    content: [
+    title: String,   // Champ pour le titre du quiz
+    theme: String,   // Champ pour le thème du quiz
+    content: [       // Champ pour le contenu du quiz, qui est un tableau d'objets
         {
-            question: String,
-            answers: [
-                { id: Number, text: String, correct: Boolean },
-                { id: Number, text: String, correct: Boolean },
-                { id: Number, text: String, correct: Boolean },
-                { id: Number, text: String, correct: Boolean }
+            question: String,   // Champ pour la question du quiz
+            answers: [          // Champ pour les réponses possibles, qui est un tableau d'objets
+                { 
+                    id: Number,    // Champ pour l'identifiant de la réponse
+                    text: String,  // Champ pour le texte de la réponse
+                    correct: Boolean // Champ pour indiquer si la réponse est correcte ou non
+                },
+                // Les autres réponses possibles sont également définies de la même manière
             ]
         }
     ]
 })
 
+// Créer un modèle Mongoose à partir du schéma, nommé "Quizz" qui sera utilisé pour interagir avec la base de données
 const QuizzModel = mongoose.model("Quizz", QuizzSchema)
 
+// Exporter le modèle pour pouvoir l'utiliser dans d'autres parties de l'application
 module.exports = QuizzModel
